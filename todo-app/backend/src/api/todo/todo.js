e7dc8a250e05efaeb0e0caf2b1132d3213f991ad
Vitor <vih.frez@gmail.com>
2018-05-10 23:35:44 -0300

    (todoApp)Varios

--- todo-app/backend/src/api/todo/todo.js
+++ todo-app/backend/src/api/todo/todo.js
@@ -1,10 +1,10 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const todoSchema =new mongoose.schema({
+const todoSchema = new mongoose.Schema({
     description: { type: String, required: true},
-    done: { type: Boolean, require: true, default: false},
-    createdAt: { Type: Date, default: Date.now}
+    done: { type: Boolean, required: true, default: false},
+    createdAt: { type: Date, default: Date.now }
 })
 
 module.exports = restful.model('Todo', todoSchema)
